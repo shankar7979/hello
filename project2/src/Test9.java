cfbf55861350d000dbb84f0482f989c96fb3126a
shankar <shankar7979@gmail.com>
2018-05-26 18:54:09 +0530

    removed1

--- project2/src/Test9.java
+++
@@ -1,7 +0,0 @@
-class Test9 {
-
-public static void main(String args[]){
-System.out.print("hello");
-}
-
-}
--- project2/src/test6.java
+++
@@ -1,6 +0,0 @@
-class test6{
-	public static void main(String args[]){
-	    int b=0b111;
-	    System.out.println(b);
-		}
-	}
